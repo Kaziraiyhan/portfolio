d4768e2d3063b1f3a0dcbab9a841be3f2eebf54d
Kaziraiyhan <112329938+Kaziraiyhan@users.noreply.github.com>
2023-06-23 21:15:54 +0600

    upto education

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,10 +38,7 @@ module.exports = {
         },
       },
       fontFamily: {
-        // 'sans': ['Poppins','ui-sans-serif', 'system-ui', ],
         'sans': ["Inter","sans-serif", ],
-        // 'sans': ["system-ui", "-apple-system","Segoe UI", "Roboto","Helvetica Neue", "Arial","Noto Sans","Liberation Sans","sans-serif","Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji", ],
-        // 'sans': [ "SFMono-Regular", "Menlo,Monaco,Consolas" ,"Liberation Mono","Courier New", "monospace ", ],
 
         
     
