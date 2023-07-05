e26fade8d3ddfcea49073e701ccf1598c7d3eec6
Kaziraiyhan <112329938+Kaziraiyhan@users.noreply.github.com>
2023-07-05 11:49:43 +0600

    Clients slider add

--- public/js/script.js
+++ public/js/script.js
@@ -28,35 +28,35 @@ menu_bar.addEventListener("click", function() {
  
     $('.slaider').slick({
        
-        slidesToShow: 3,
+        slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
+        // autoplay: true,
+        // autoplaySpeed: 5000,
         prevArrow : '<button class="slick_prev" aria-label="Previous" type="button"><i class="fa-solid fa-angle-left"></i></button>',
         nextArrow: '<button class="slick_next" aria-label="Next" type="button"><i class="fa-solid fa-angle-right"></i></button>',
 
-        responsive: [
-          {
-            breakpoint: 1165,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1,
-              infinite: true,
+        // responsive: [
+        //   {
+        //     breakpoint: 1165,
+        //     settings: {
+        //       slidesToShow: 1,
+        //       slidesToScroll: 1,
+        //       infinite: true,
         
-            }
-          },
-          {
-            breakpoint: 770,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-              }
-          },
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 770,
+        //     settings: {
+        //       slidesToShow: 1,
+        //       slidesToScroll: 1
+        //       }
+        //   },
    
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
+        //   // You can unslick at a given breakpoint now by adding:
+        //   // settings: "unslick"
+        //   // instead of a settings object
+        // ]
       
 
       });    
